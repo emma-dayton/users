838391393f6f552308cc917f9c04d643142527cf
Emma Dayton <emma.dayton85@gmail.com>
2019-03-12 14:33:37 -0700

    update README, add users/add route to server, finish /user/<num>/show route on server.py, update templates to better match wireframes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Users - Coding Dojo Assignment
-Here you'll find an example of a complete CRUD app.
+Here you'll find an example of a complete CRUD app. This app will allow users to add, edit, or delete a user, as well as
+display the table of users or the values for an individual user.
 Skills used:
 * Python
 * Flask
--- server.py
+++ server.py
@@ -3,19 +3,22 @@ from mysqlconnection import connectToMySQL
 app = Flask(__name__)
 app.secret_key = 'e7ef0a420bb411f5e8222ee7345fab24'
 # our index route will handle rendering our form
-@app.route('/')
+@app.route('/users')
 def index():
-    if 'header' not in session:
-        session['header'] = 'All Users'
+    session['title'] = 'All Users'
     db = connectToMySQL('users_dojo_db')
     users = db.query_db("SELECT * FROM users;")
     print(users)
-    return render_template("all_users.html")
+    return render_template("all_users.html", users=users)
 #
 #
-@app.route('/add_user', methods=['POST'])
+@app.route('/users/add')
+def add():
+    session['title'] = 'Add User'
+    return render_template("add_user.html")
+
+@app.route('/users/add/add_user', methods=['POST'])
 def add_user():
-    session['header'] = 'Add a New User'
     db = connectToMySQL('users_dojo_db')
     data = {
     'fn': request.form('first_name'),
@@ -26,8 +29,18 @@ def add_user():
     query = '''INSERT INTO users_dojo_db (first_name, last_name, email,
     created_at, updated_at) VALUES(%(fn)s, %(ln)s, %(email)s, now(), now())'''
     db.query_db(query, data)
-    return redirect('/')
+    return redirect('/users')
 
+@app.route('/users/<num>/show')
+def show_user(num):
+    num = num
+    db = connectToMySQL('users_dojo_db')
+    query = f'SELECT * FROM users WHERE id = {num} ;'
+    user = db.query_db(query)
+    user = user[0]
+    print(user)
+    session['title'] = f"{user['first_name']} {user['last_name']}"
+    return render_template('user.html', user=user)
 
 
 if __name__ == "__main__":
